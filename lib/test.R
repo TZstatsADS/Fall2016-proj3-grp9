6cfc84ff80250852f1e44543d3595316b1aa2ddd
valenese <rl2633@tc.columbia.edu>
2016-11-01 23:00:18 -0400

    test

--- lib/test.R
+++ lib/test.R
@@ -1,26 +1,45 @@
-######################################################
-### Fit the classification model with testing data ###
-######################################################
+library(kernlab)
+library(e1071)
+library(rpart)
+library(class)
+library(gbm)
+data = read.csv(file.choose(), header = T)
+dim(data)
+head(data)[,1:6]
+data$Num_Categories[1:1000] = 1
+data$Num_Categories[1001:2000] = 0
+data$Categories[data$Num_Categories==0] = "dog"
+data$Categories[data$Num_Categories==1] = "chicken"
+dim(data)
+norm2_test = data
+head(norm2_test)[1:9]
 
-### Author: Yuting Ma
-### Project 3
-### ADS Spring 2016
+attach(norm2_test)
+####cleaning data####
+norm2_test <- subset(norm2_test, select=-c(X,X0))
+x_test <- subset(norm2_test, select = -Categories)
+x_test <- subset(x_test, select = -Num_Categories)
+y_test <- subset(norm2_test, select = Num_Categories)
 
-test <- function(fit_train, dat_test){
-  
-  ### Fit the classfication model with testing data
-  
-  ### Input: 
-  ###  - the fitted classification model using training data
-  ###  -  processed features from testing images 
-  ### Output: training model specification
-  
-  ### load libraries
-  library("gbm")
-  
-  pred <- predict(fit_train$fit, newdata=dat_test, 
-                  n.trees=fit_train$iter, type="response")
-  
-  return(as.numeric(pred> 0.5))
-}
 
+##### PCA #########
+pca_test <- prcomp(x_test,scale = T)
+head(pca_test$x)
+plot(pca_test,type="lines")
+
+pr_var <- (pca_test$sdev)^2
+prop_varex <- pr_var/sum(pr_var)
+plot(prop_varex, xlim=c(1,50),xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     type = "b")
+
+# select the number of PC
+pca_xtest = pca_test$x[,1:5]
+head(pca_xtest)
+
+
+
+######Testing Model Performance#####
+pred_test <- predict(best.svm,pca_xtest)
+accur_test = sum(pred_test == as.vector(as.matrix(y_test)))/2000
+accur_test
