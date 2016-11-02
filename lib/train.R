3d5dc8c971671278a9a9e8309d19a1811d30d457
valenese <rl2633@tc.columbia.edu>
2016-11-02 09:35:33 -0400

    train

--- lib/train.R
+++ lib/train.R
@@ -2,54 +2,46 @@ library(kernlab)
 library(e1071)
 library(rpart)
 library(class)
-data = read.csv(file.choose(), header = T)
-dim(data)
-head(data)[,1:6]
-data$Num_Categories[1:1000] = 1
-data$Num_Categories[1001:2000] = 0
-data$Categories[data$Num_Categories==0] = "dog"
-data$Categories[data$Num_Categories==1] = "chicken"
-dim(data)
-norm2 = data
-head(norm2)[1:9]
+library(gbm)
 
-attach(norm2)
-####cleaning data####
-norm2 <- subset(norm2, select=-c(X,X0))
-x <- subset(norm2, select = -Categories)
-x <- subset(x, select = -Num_Categories)
-y <- subset(norm2, select = Num_Categories)
+### Build the Trining function 
+Train = function(data, Y){
+  data$Y[1:1000] = 1
+  data$Y[1001:2000] = 0
+  norm2 = data
+  ####cleaning data####
+  norm2 <- subset(norm2, select=-c(X,X0))
+  x <- subset(norm2, select = -Y)
+  y <- subset(norm2, select = Y)
+  
+  ##### PCA #########
+  pca <- prcomp(x,scale = T)
+  
+  # make prince compoent of 8 # 
+  pca_x = pca$x[,1:5]
+  
+  ### svm classification after PCA
+  # cross validation of 20 fold
+  tc <- tune.control(cross = 20)
+  Cs = c(.001,.01,.1,.5,1)
+  gammas = 10^(-2:2)
+  degres = c(0,1)
+  coef = c(0,1)
+  cv_svmTune <- tune.svm(pca_x, y =y, cost = Cs, gamma = gammas,degree = degres,coef0 = coef,
+                         tunecontrol = tc)
+  summary(cv_svmTune)
+  ###### best adv model 
+  p = cv_svmTune$best.parameters
+  best.svm = svm(pca_x,y,cost = p[,4], gamma = p[,2],degree = p[,1],coef0 = p[,3])
+  
+  ####baseline model###
+  gbm_model <- gbm.fit(pca_x,as.vector(as.matrix(y)),distribution = "bernoulli", n.trees = 100)
+  return(list(gbm.base=gbm_model,svm.adv=best.svm))
+  
+}
 
-
-##### PCA #########
-pca <- prcomp(x,scale = T)
-head(pca$x)
-plot(pca,type="lines")
-
-# make prince compoent of 8 # 
-pca_x = pca$x[,1:5]
-head(pca_x)
-
-pr_var <- (pca$sdev)^2
-prop_varex <- pr_var/sum(pr_var)
-plot(prop_varex, xlim=c(1,50),xlab = "Principal Component",
-     ylab = "Proportion of Variance Explained",
-     type = "b")
-
-### svm classification after PCA
-# cross validation of 20 fold
-tc <- tune.control(cross = 20)
-Cs = c(.001,.01,.1,.5,1)
-gammas = 10^(-2:2)
-degres = c(0,1)
-coef = c(0,1)
-cv_svmTune <- tune.svm(pca_x, y =y, cost = Cs, gamma = gammas,degree = degres,coef0 = coef,
-                       tunecontrol = tc)
-summary(cv_svmTune)
-
-# best model 
-p = cv_svmTune$best.parameters
-best.svm = svm(pca_x,y,cost = p[,4], gamma = p[,2],degree = p[,1],coef0 = p[,3],type = "C-classification")
-pred_svm <- predict(best.svm,pca_x)
-accur = sum(pred_svm == as.vector(as.matrix(y)))/2000
-accur
+###  train model 
+norm2 = read.csv(file.choose(), header = T)
+norm2$Num_Categories[1:1000] = 1
+norm2$Num_Categories[1001:2000] = 0
+model = Train(data = norm2, Y = Num_Categories)
